51750962acb44168db6e313c36b9951640038451
鈴木 嵩博 <halsuzukitakahiro@gmail.com>
2021-10-11 18:12:39 +0900

    鈴木嵩博【修正】：ランクによって人員のステータスが変わるように修正。

--- C - Keeper/Assets/Suzuki/Scripts/CharacterPanel.cs
+++ C - Keeper/Assets/Suzuki/Scripts/CharacterPanel.cs
@@ -8,18 +8,83 @@ public class CharacterPanel : MonoBehaviour
 {
     public int research, work;  // キャラクターのパラメータ
     public Text researchText, workText;
+    public string rank;
 
     private int index;
     public Image image;
 
+    public void SetRank(int Rank)
+    {
+        if(Rank <= 1)
+        {
+            research = Random.Range(0, 2);
+            work = Random.Range(0, 2);
+
+            researchText.text = "研究　" + RankTransfer(research);
+            workText.text = "作業　" + RankTransfer(work);
+        }
+        else if(Rank == 2)
+        {
+            research = Random.Range(0, 3);
+            work = Random.Range(0, 3);
+
+            researchText.text = "研究　" + RankTransfer(research);
+            workText.text = "作業　" + RankTransfer(work);
+        }
+        else if(Rank == 3)
+        {
+            research = Random.Range(1, 4);
+            work = Random.Range(1, 4);
+
+            researchText.text = "研究　" + RankTransfer(research);
+            workText.text = "作業　" + RankTransfer(work);
+        }
+        else if (Rank == 4)
+        {
+            research = Random.Range(2, 5);
+            work = Random.Range(2, 5);
+
+            researchText.text = "研究　" + RankTransfer(research);
+            workText.text = "作業　" + RankTransfer(work);
+        }
+        else
+        {
+            research = Random.Range(3, 6);
+            work = Random.Range(3, 6);
+
+            researchText.text = "研究　" + RankTransfer(research);
+            workText.text = "作業　" + RankTransfer(work);
+        }
+    }
+
+    public string RankTransfer(int Param)
+    {
+        string tmp;
+
+        if (Param <= 0)
+            tmp = "E";
+        else if (Param == 1)
+            tmp = "D";
+        else if (Param == 2)
+            tmp = "C";
+        else if (Param == 3)
+            tmp = "B";
+        else if (Param == 4)
+            tmp = "A";
+        else
+            tmp = "S";
+
+        return tmp;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        research = Random.Range(0, 101);
-        work = Random.Range(0, 101);
+        //research = Random.Range(0, 7);
+        //work = Random.Range(0, 7);
 
-        researchText.text = "研究　" + research.ToString();
-        workText.text = "作業　" + work.ToString();
+        //researchText.text = "研究　" + research.ToString();
+        //workText.text = "作業　" + work.ToString();
 
         index = Random.Range(1, 6);
         image = transform.Find("Character/Image").GetComponent<Image>();
--- C - Keeper/Assets/Suzuki/Scripts/PanelManager.cs
+++ C - Keeper/Assets/Suzuki/Scripts/PanelManager.cs
@@ -8,6 +8,8 @@ using UnityEngine.SceneManagement;
 public class PanelManager : MonoBehaviour
 {
     public GameObject CharacterManager;
+    [SerializeField, Range(1, 5)]
+    private int rank;
 
     private const int PANEL_MAX = 5;
     private GameObject[] Panels = new GameObject[PANEL_MAX];
@@ -71,6 +73,7 @@ public class PanelManager : MonoBehaviour
         for(int i = 0; i < PANEL_MAX; i++)
         {
             Panels[i] = Instantiate((GameObject)Resources.Load("Panel"));
+            Panels[i].GetComponent<CharacterPanel>().SetRank(rank);
         }
 
         for (int i = 0; i < 2; i++)
