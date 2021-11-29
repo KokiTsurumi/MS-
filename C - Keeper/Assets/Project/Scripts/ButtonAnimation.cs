5646096ba2d4f7a2a6c32bdbfe55d992e47237be
鶴見 晃紀 <tsurumi.kouki.as@gmail.com>
2021-11-29 20:27:44 +0900

    鶴見 晃紀【追加】：アイコンにメーション追加

---
+++ C - Keeper/Assets/Project/Scripts/ButtonAnimation.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAnimation : MonoBehaviour
+{
+    public Animator animator;
+    
+    public void OnEnterButton()
+    {
+        animator.SetBool("Start",true);
+        
+    }
+
+    public void OnExitButton()
+    {
+        animator.SetBool("Start", false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
